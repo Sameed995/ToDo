5fc7a2a1df9ea1177a804169d3735a68b29fbabe
Sameed995 <sameedshaikh445@gmail.com>
2025-11-28 00:16:52 +0530

    frontend issue fix:

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://todo-jdiu.onrender.com';
+const API_URL = 'https://todo-jdiu.onrender.com/api/auth';
 
 export const registerUser = async (formData) => {
   return await axios.post(`${API_URL}/register`, formData);
